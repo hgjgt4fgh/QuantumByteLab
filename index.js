6dde72808329123b6b804941772fa8f14feba34e
hgjgt4fgh <gs1hjghjgdj@hotmail.com>
2024-05-03 14:15:14 +0700

    hello world

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-function isPalindrome(str) {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
+function trap(height) {
+  let totalWater = 0;
+  let left = 0;
+  let right = height.length - 1;
+  let maxLeft = 0;
+  let maxRight = 0;
+  while (left < right) {
+    if (height[left] < height[right]) {
+      if (height[left] >= maxLeft) {
+        maxLeft = height[left];
+      } else {
+        totalWater += maxLeft - height[left];
+      }
+      left++;
+    } else {
+      if (height[right] >= maxRight) {
+        maxRight = height[right];
+      } else {
+        totalWater += maxRight - height[right];
+      }
+      right--;
+    }
+  }
+  return totalWater;
 }
-
-console.log(isPalindrome("racecar")); // true
